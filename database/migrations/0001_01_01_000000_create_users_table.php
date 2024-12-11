70f20a47318ba987f4ea3e9c30e041156d1113ef
Botir Ismatov <botirismatov@MacBook-Pro-Botir.local>
2024-12-11 20:32:41 +0500

    role migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('status')->default(1);
+            $table->foreignId('role_id')->default(4);
             $table->rememberToken();
             $table->timestamps();
         });
